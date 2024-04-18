66923d7f3a02cd0062b5af23f0a9f29440c94d66
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-04-18 23:57:50 +0600

    added env local

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: "real-estate-b9-a9.firebaseapp.com",
-  projectId: "real-estate-b9-a9",
-  storageBucket: "real-estate-b9-a9.appspot.com",
-  messagingSenderId: "317433483114",
-  appId: "1:317433483114:web:fd19f6ec1157aebf8e9f13"
+  apiKey: import.meta.envVITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
